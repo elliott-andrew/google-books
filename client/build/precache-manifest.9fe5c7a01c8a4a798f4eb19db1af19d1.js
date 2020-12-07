6320aba4f62ecd6d1992f4785a94b99a54f6d4d7
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-12-06 21:38:58 -0500

    Ran build to solvfix error when running npm start

---
+++ client/build/precache-manifest.9fe5c7a01c8a4a798f4eb19db1af19d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97504dc265d1fa6a8c653d9982f979e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb3737cd966b19786a07",
+    "url": "/static/css/main.cafcb57e.chunk.css"
+  },
+  {
+    "revision": "1a2c8de1d357271a38ec",
+    "url": "/static/js/2.16c399be.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.16c399be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb3737cd966b19786a07",
+    "url": "/static/js/main.8dd59205.chunk.js"
+  },
+  {
+    "revision": "bbac952799e545852822",
+    "url": "/static/js/runtime-main.084f65f5.js"
+  }
+]);
