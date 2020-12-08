f182198b34593230afe1f8b7aa4f627af6d87487
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-12-07 22:24:44 -0500

    Created constant to hold search item and set method

--- client/src/Pages/Search.js
+++ client/src/Pages/Search.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TextArea, FormBtn } from '../Components/Form';
 import bookIcon from "../assets/index";
 
 const Search = () => {
+    const [formObject, setFormObject] = useState(
+        {
+            title: "",
+            results: []
+        }
+    );
+
     return (
         <form>
             <img src={bookIcon} alt="Books Icon" style={{ width: "200px", margin: "0 auto", marginTop: "50px", display: "block" }} />
