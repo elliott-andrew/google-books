7007bc3a563311dc8cc7a8dce70952e5f9c8d11b
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-12-07 23:34:09 -0500

    Updated book component to when no description is returned

--- client/src/Components/Book/index.js
+++ client/src/Components/Book/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import "./style.css"
 
 const Book = ({ title, authors, description, thumbnail }) => {
     return (
         <div className="book-info" key={title}>
+            <img alt={title + " - " + authors} src={thumbnail} />
             <h2>{title}</h2>
             <h3>{authors}</h3>
-            <p>{description}</p>
-            <img alt={title + " - " + authors} src={thumbnail} />
+            <p>{description ? description : "No desciption found."}</p>
         </div>
     );
 };
