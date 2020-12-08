f0b1963f2afb7ccfc2af60f1d12e9ceb41f2bc58
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-12-07 23:40:30 -0500

    Updated book component

--- client/src/Components/Book/index.js
+++ client/src/Components/Book/index.js
@@ -3,11 +3,13 @@ import "./style.css"
 
 const Book = ({ title, authors, description, thumbnail }) => {
     return (
-        <div className="book-info" key={title}>
-            <img alt={title + " - " + authors} src={thumbnail} />
-            <h2>{title}</h2>
-            <h3>{authors}</h3>
-            <p>{description ? description : "No desciption found."}</p>
+        <div id="allResults">
+            <div className="book-info" key={title}>
+                <img alt={title + " - " + authors} src={thumbnail} />
+                <h2>{title}</h2>
+                <h3>{authors}</h3>
+                <p>{description ? description : "No desciption found."}</p>
+            </div>
         </div>
     );
 };
